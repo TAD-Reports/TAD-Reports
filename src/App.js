17fc4112d1f5d1e43f8b08880381fbcd36d84010
MarkSalem804 <125126248+MarkSalem804@users.noreply.github.com>
2023-05-02 15:53:57 +0800

    "ngongit ni nyud"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Route, Routes } from "react-router-dom";
-import Dashboard from "./pages/app-form";
+import AppForm from "./pages/app-form";
 import RequireAuth from "./components/RequireAuth";
 import Missing from "./pages/missing";
 import HRPage from "./pages/hr-page";
 import SignIn from "./pages/authenticate/sign-in";
+import Sidebar from "./components/Sidebar/index.js";
+import "./components/Sidebar/sidebar.css"
 
 const ROLES = {
   User: 2001,
@@ -15,11 +17,11 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<SignIn />} />
+        <Route path="/" element={<Sidebar />} />
         <Route path="/sign-in" element={<SignIn />} />
 
         <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/appform" element={<AppForm />} />
           <Route path="/hrpage" element={<HRPage />} />
         </Route>
 
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./sidebar.css"
+import { Box } from '@mui/material';
+
+function Sidebar() {
+  return (
+    <Box className='Try'>
+        Hello Matthew
+    </Box>
+  )
+}
+
+export default Sidebar;
