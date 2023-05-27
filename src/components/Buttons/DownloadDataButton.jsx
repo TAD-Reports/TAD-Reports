b487f24e31e7a0a05502c297530e9a0378c53c16
Wandering Wizard <107069990+hernandezraymondm@users.noreply.github.com>
2023-05-28 07:49:30 +0800

    nursery report

--- src/components/Buttons/DownloadDataButton.jsx
+++ src/components/Buttons/DownloadDataButton.jsx
@@ -1,47 +1,13 @@
 import React from "react";
 import { Button } from "@mui/material";
 import DownloadIcon from "@mui/icons-material/Download";
-import * as XLSX from "xlsx";
 import PropTypes from "prop-types";
 
-export default function DownloadDataButton({ data, moduleName }) {
-  const handleDownload = () => {
-    if (!data || data.length === 0) {
-      console.log("No data available to export.");
-      return;
-    }
-
-    const workbook = XLSX.utils.book_new();
-    const worksheet = XLSX.utils.json_to_sheet(data);
-
-    XLSX.utils.book_append_sheet(workbook, worksheet, "Data");
-    const excelBuffer = XLSX.write(workbook, {
-      bookType: "xlsx",
-      type: "array",
-    });
-
-    const blob = new Blob([excelBuffer], {
-      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-    });
-    const filename = `${moduleName}${data[0].report_date}.xlsx`;
-    if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-      // For IE browser
-      window.navigator.msSaveOrOpenBlob(blob, filename);
-    } else {
-      // For other browsers
-      const url = window.URL.createObjectURL(blob);
-      const link = document.createElement("a");
-      link.href = url;
-      link.download = filename;
-      link.click();
-      window.URL.revokeObjectURL(url);
-    }
-  };
-
+export default function DownloadDataButton({ downloadData }) {
   return (
     <Button
       variant="contained"
-      onClick={handleDownload}
+      onClick={downloadData}
       sx={{
         height: 50,
         width: 200,
@@ -60,12 +26,10 @@ export default function DownloadDataButton({ data, moduleName }) {
 }
 
 DownloadDataButton.defaultProps = {
-  data: null,
-  moduleName: "",
+  downloadData: [],
 };
 
 DownloadDataButton.propTypes = {
   // eslint-disable-next-line react/forbid-prop-types
-  data: PropTypes.object,
-  moduleName: PropTypes.bool,
+  downloadData: PropTypes.func,
 };
--- src/pages/Nursery.jsx
+++ src/pages/Nursery.jsx
@@ -1,4 +1,11 @@
+/* eslint-disable no-nested-ternary */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-plusplus */
+/* eslint-disable dot-notation */
 import React, { useState } from "react";
+// import * as XLSX from "xlsx";
+import ExcelJS from "exceljs";
 import {
   Box,
   Divider,
@@ -108,6 +115,172 @@ export default function Nursery() {
 
   const clearFileName = () => {
     setFileName("");
+  };
+
+  const handleDownload = () => {
+    if (!nurseryData || nurseryData.length === 0) {
+      console.log("No data available to export.");
+      return;
+    }
+
+    const a1 = nurseryData.filter(
+      (data) => data.nurseries === "Maintained" && data.funded_by === "PhilFIDA"
+    );
+    const a2 = nurseryData.filter(
+      (data) => data.nurseries === "Maintained" && data.funded_by === "LGU"
+    );
+    const a3 = nurseryData.filter(
+      (data) =>
+        data.nurseries === "Established" && data.funded_by === "PhilFIDA"
+    );
+    const a4 = nurseryData.filter(
+      (data) => data.nurseries === "Established" && data.funded_by === "LGU"
+    );
+
+    const workbook = new ExcelJS.Workbook();
+    const sheets = [
+      { name: "Maintained (PhilFIDA)", data: a1 },
+      { name: "Maintained (LGU)", data: a2 },
+      { name: "Established (PhilFIDA)", data: a3 },
+      { name: "Established (LGU)", data: a4 },
+    ];
+
+    const headers = [
+      "Report Date",
+      "Region",
+      "Province",
+      "Municipality",
+      "Barangay",
+      "Complete Name of Cooperator/ Organization",
+      "Date Established",
+      "Area in Hectares (ha)",
+      "Variety Used",
+      "Period of MOA",
+      "Remarks",
+    ];
+
+    sheets.forEach((sheet) => {
+      const worksheet = workbook.addWorksheet(sheet.name);
+
+      // Merge cells C to J in rows 1 to 3
+      worksheet.mergeCells("C1:J1");
+      worksheet.mergeCells("C2:J2");
+      worksheet.mergeCells("C3:J3");
+
+      worksheet.getCell("F1").alignment = { horizontal: "center" };
+      worksheet.getCell("F1").value = "Regional Office _____________";
+
+      worksheet.getCell("F2").alignment = { horizontal: "center" };
+      worksheet.getCell("F2").value = "Monthly Report of Technical Assistance";
+
+      worksheet.getCell("F3").alignment = { horizontal: "center" };
+      worksheet.getCell("F3").value = "For the month of ______________";
+
+      worksheet.getCell("A4").value = `Form A.${
+        sheets.indexOf(sheet) + 1
+      }: Report on Abaca Nurseries ${sheet.name}`;
+      worksheet.getCell("A4").alignment = { horizontal: "left" };
+
+      worksheet.getRow(5).values = headers;
+      worksheet.getRow(5).height = 65;
+
+      const filteredData = sheet.data;
+
+      filteredData.forEach((data) => {
+        const rowData = [
+          data.report_date,
+          data.region,
+          data.province,
+          data.municipality,
+          data.barangay,
+          data.complete_name_of_cooperator_organization,
+          data.date_established,
+          data.area_in_hectares_ha,
+          data.variety_used,
+          data.period_of_moa,
+          data.remarks,
+        ];
+        worksheet.addRow(rowData);
+      });
+
+      const columnWidths = [
+        { width: 20 },
+        { width: 20 },
+        { width: 20 },
+        { width: 20 },
+        { width: 20 },
+        { width: 40 },
+        { width: 20 },
+        { width: 20 },
+        { width: 20 },
+        { width: 20 },
+        { width: 40 },
+      ];
+
+      worksheet.columns = columnWidths;
+
+      // Make rows 1 to 5 bold
+      for (let i = 1; i <= 5; i++) {
+        worksheet.getRow(i).font = { bold: true };
+      }
+
+      // Set border for the table
+      const startRow = 5;
+      const startCol = 1; // Column A
+      const endRow = startRow + filteredData.length;
+      const endCol = 11; // Column K
+
+      for (let row = 1; row <= endRow; row++) {
+        for (let col = startCol; col <= endCol; col++) {
+          const cell = worksheet.getCell(row, col);
+          if (row >= 5) {
+            cell.alignment = { horizontal: "center" };
+            cell.border = {
+              top: { style: "thin" },
+              right: { style: "thin" },
+              bottom: { style: "thin" },
+              left: { style: "thin" },
+            };
+          }
+          if (row <= 4) {
+            // Set white background color for cells A1 to K4
+            cell.fill = {
+              type: "pattern",
+              pattern: "solid",
+              fgColor: { argb: "FFFFFFFF" }, // Set the desired color code here (white)
+            };
+          } else if (row === startRow) {
+            // Apply fill color to headers
+            cell.fill = {
+              type: "pattern",
+              pattern: "solid",
+              fgColor: { argb: "ffa4ffa4" }, // Set the desired color code here
+            };
+          }
+        }
+      }
+    });
+
+    const filename = `Nursery_report${nurseryData[0].report_date}.xlsx`;
+
+    workbook.xlsx.writeBuffer().then((buffer) => {
+      const blob = new Blob([buffer], {
+        type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      });
+
+      if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+        // For IE browser
+        window.navigator.msSaveOrOpenBlob(blob, filename);
+      } else {
+        // For other browsers
+        const url = window.URL.createObjectURL(blob);
+        const link = document.createElement("a");
+        link.href = url;
+        link.download = filename;
+        link.click();
+        window.URL.revokeObjectURL(url);
+      }
+    });
   };
 
   return (
@@ -246,7 +419,7 @@ export default function Nursery() {
           importFunction={handleFile}
           clearFileName={clearFileName}
         />
-        <DownloadDataButton data={nurseryData} moduleName="Nursery" />
+        <DownloadDataButton downloadData={handleDownload} />
       </Box>
       {error}
       {buttonError}
